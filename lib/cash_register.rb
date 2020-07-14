d2d6875beb73b841987705f556fc3e7f9a4445de
Jackson Reynolds <jwreynold@gmail.com>
2020-07-14 18:40:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,7 +6,16 @@ class CashRegister
 
   def initialize(discount = 0)
     @discount =  1 - discount.to_f/100
-
+    @total = 0
   end #initialize
+
+  def add_item(item_title, price)
+
+  end #add_item
+
+  def apply_discount
+    @total = @discount*@total
+    "some message"
+  end #apply_discount
 
 end #CashRegister
