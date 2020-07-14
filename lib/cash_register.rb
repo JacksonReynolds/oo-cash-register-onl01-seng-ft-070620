45d02283a17e3693e74413640790616537b96a02
Jackson Reynolds <jwreynold@gmail.com>
2020-07-14 18:38:16 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,12 @@
+require 'pry'
 
+  attr_reader :total, :discount
+
+class CashRegister
+
+  def initialize(discount = 0)
+    @discount =  1 - discount.to_f/100
+    
+  end #initialize
+
+end #CashRegister
